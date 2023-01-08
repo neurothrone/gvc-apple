db77e97d8ef7ea84b8e71fb983aa75a4554f1218
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-01-08 06:09:28 +0100

    remove color on segmented control

--- GVC-Apple-Multiplatform/AppMain.swift
+++ GVC-Apple-Multiplatform/AppMain.swift
@@ -9,34 +9,10 @@ import SwiftUI
 
 @main
 struct AppMain: App {
-  
-  init() {
-    configureSegmentedControl()
-  }
-  
   var body: some Scene {
     WindowGroup {
       ContentView()
         .environment(\.managedObjectContext, CoreDataProvider.shared.viewContext)
     }
-  }
-}
-
-extension AppMain {
-  private func configureSegmentedControl() {
-    UISegmentedControl.appearance()
-      .selectedSegmentTintColor = UIColor(Color.accentColor.opacity(0.5))
-    UISegmentedControl.appearance()
-      .backgroundColor = UIColor(Color.accentColor.opacity(0.3))
-    UISegmentedControl.appearance()
-      .setTitleTextAttributes(
-        [.foregroundColor: UIColor(Color.primary)],
-        for: .selected
-      )
-    UISegmentedControl.appearance()
-      .setTitleTextAttributes(
-        [.foregroundColor: UIColor(Color.secondary)],
-        for: .normal
-      )
   }
 }
